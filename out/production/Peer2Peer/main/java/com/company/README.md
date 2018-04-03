202bfb7b3caacb45ae6d6fcc3d2b7af3d22da269
= <geoffrey.geofe@gmail.com>
2018-04-03 03:01:40 -0500

    peer to peer program

---
+++ out/production/Peer2Peer/main/java/com/company/README.md
@@ -0,0 +1,4 @@
+Napster Style Peer to Peer File Sharing System
+-->Run the server in the background.
+-->User enters the socket number for the client to connect to the Server.
+-->All peers register there files to the server.
---
+++ peer2/src/peer2/Message.java
@@ -0,0 +1,15 @@
+package peer2;
+
+public class Message {
+    String id;
+    String ip;
+    String command;
+    String data;
+    public Message(String id, String ip, String command, String data)
+    {
+        this.id=id;
+        this.command=command;
+        this.ip=ip;
+        this.data=data;
+    }
+}
